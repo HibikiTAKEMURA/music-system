0f64df86e814fd00795025f1c21fee16f225145c
HibikiTAKEMURA <bvs.jazzmusic.valvetrombone@gmail.com>
2025-01-07 01:09:58 +0900

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
+
 import './App.css'
 import Home from './pages/home'
 import Notfound from './pages/error/notFound'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
+const APP_NAME: String = "music-system";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="*" element={ <Notfound /> } />
+        <Route path={APP_NAME + '/home'} element={<Home />} />
+        <Route path={APP_NAME + '*'} element={ <Notfound /> } />
       </Routes>
     </BrowserRouter>
   )
